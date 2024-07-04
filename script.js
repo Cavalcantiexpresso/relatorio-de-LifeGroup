e9adb0bdb40b33ef0cb6f67174ccee68d35e7a6b
LUCIANO PAIVA CAVALCANTI <98571732+Cavalcantiexpresso@users.noreply.github.com>
2024-07-04 17:14:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// script.js
-
 function sendReport() {
-    // Coletar os valores dos campos de entrada
+    
     const membrosLifeGroup = document.getElementById('membros-life-group').value;
     const convidadosLifeGroup = document.getElementById('convidados-life-group').value;
     const membrosTadel = document.getElementById('membros-tadel').value;
@@ -13,28 +11,27 @@ function sendReport() {
     const mdas = document.getElementById('mdas').value;
     const ges = document.getElementById('ges').value;
 
-    // Criar a mensagem para enviar via WhatsApp e email
+    
     const mensagem = `RELATÓRIO:\n\nMembros do Life Group: ${membrosLifeGroup}\nMembros presentes no Tadel: ${convidadosLifeGroup}\nMembros presentes: ${membrosTadel}\nConvidados presentes: ${convidadosTadel}\nCrianças (0 a 11): ${criancasTadel}\nTotal Presentes: ${totalTadel}\nTOTAL DE PRESENTES NO CULTO: ${totalCultoGeral}\n\nDiscipuladores: ${discipuladores}\nMDA's: ${mdas}\nGE's: ${ges}`;
 
-    // Codificar a mensagem para URL
+
     const mensagemCodificada = encodeURIComponent(mensagem);
 
-    // Número de telefone para enviar o relatório via WhatsApp
-    const numeroTelefone = '5581999440404'; // Número no formato internacional
+ 
+    const numeroTelefone = '5581999440404';
 
     // URL do WhatsApp API
     const whatsappURL = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text=${mensagemCodificada}`;
 
-    // Email para enviar o relatório
+   
     const email = 'ifenixtecnologia@gmail.com';
     const assunto = 'Relatório de Membros da Igreja';
     const emailBody = encodeURIComponent(mensagem);
     const mailtoURL = `mailto:${email}?subject=${encodeURIComponent(assunto)}&body=${emailBody}`;
 
-    // Abrir a URL do WhatsApp em uma nova janela
     window.open(whatsappURL, '_blank');
 
-    // Abrir a URL do email em uma nova janela
+  
     window.open(mailtoURL, '_blank');
 }
 
